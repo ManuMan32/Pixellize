895c0da4d998164c0e8a954f7e686b5463bd10e0
ManuMan32 <141773224+ManuMan32@users.noreply.github.com>
2024-05-11 12:26:53 -0300

    Add canvas grid

--- src/components/editor/canvasbox/canvasbox.tsx
+++ src/components/editor/canvasbox/canvasbox.tsx
@@ -1,19 +1,32 @@
 import './canvasbox.css';
 import { useState, useEffect } from 'react';
 interface CanvasBox { rect: DOMRect | undefined, x: number, y: number }
-interface CanvasMovement { x: number, y: number }
+interface Coordinates { x: number, y: number }
 const canvasBox: CanvasBox = {
   rect: undefined,
   x: 0,
   y: 0,
 }
 const CanvasBox: React.FC = () => {
-  const [canvasMovement, setCanvasMovement] = useState<CanvasMovement>({ x: 0, y: 0 });
+  const [canvasSize, setCanvasSize] = useState<Coordinates>({ x: 64, y: 64 });
+  const [pixelSize, setPixelSize] = useState<number>(6);
+  const [canvasMovement, setCanvasMovement] = useState<Coordinates>({ x: 0, y: 0 });
   useEffect(() => {
     const cb = document.querySelector('.editorCanvas');
     canvasBox.rect = cb?.getBoundingClientRect();
     canvasBox.x = canvasBox.rect!.x;
     canvasBox.y = canvasBox.rect!.y;
+    // Canvas grid
+    const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+    canvas.width = canvasSize.x * pixelSize;
+    canvas.height = canvasSize.y * pixelSize;
+    const ctx: CanvasRenderingContext2D = canvas!.getContext('2d') as CanvasRenderingContext2D;
+    for (let i = 0; i < canvasSize.x * pixelSize; i += pixelSize * 2) {
+      ctx.fillRect(i, 0, 1, canvasSize.y * pixelSize);
+    }
+    for (let j = 0; j < canvasSize.y * pixelSize; j += pixelSize * 2) {
+      ctx.fillRect(0, j, canvasSize.x * pixelSize, 1);
+    }
   }, [])
   function handleMouseMove(e: any) {
     if (e.buttons === 4) {
@@ -26,6 +39,8 @@ const CanvasBox: React.FC = () => {
   return (
     <div className='editorBar editorCanvas' onMouseMove={handleMouseMove}>
       <canvas id='canvas' style={{
+        width: `${canvasSize.x * pixelSize}px`,
+        height: `${canvasSize.y * pixelSize}px`,
         left: `${canvasMovement.x}px`,
         top: `${canvasMovement.y}px`
       }}></canvas>
