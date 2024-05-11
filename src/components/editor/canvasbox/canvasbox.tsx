625eea7c818c2fe55fb8657d518d970c58fadff9
ManuMan32 <141773224+ManuMan32@users.noreply.github.com>
2024-05-11 16:49:03 -0300

    Add brush tool working

--- src/components/editor/canvasbox/canvasbox.tsx
+++ src/components/editor/canvasbox/canvasbox.tsx
@@ -20,14 +20,28 @@ const CanvasBox: React.FC = () => {
   useEffect(() => {
     // Canvas grid
     const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-    canvas.width = canvasSize.x * pixelSize;
-    canvas.height = canvasSize.y * pixelSize;
     const ctx: CanvasRenderingContext2D = canvas!.getContext('2d') as CanvasRenderingContext2D;
-    for (let i = 0; i < canvasSize.x * pixelSize; i += pixelSize * 2) {
-      ctx.fillRect(i, 0, 1, canvasSize.y * pixelSize);
+    const imageData = canvas.toDataURL('image/png');
+    const image = new Image;
+    image.src = imageData;
+    image.onload = () => {
+      canvas.width = canvasSize.x * pixelSize;
+      canvas.height = canvasSize.y * pixelSize;
+      ctx.imageSmoothingEnabled = false;
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     }
-    for (let j = 0; j < canvasSize.y * pixelSize; j += pixelSize * 2) {
-      ctx.fillRect(0, j, canvasSize.x * pixelSize, 1);
+    const canvasMask: HTMLCanvasElement = document.getElementById('canvasMask') as HTMLCanvasElement;
+    const ctxMask: CanvasRenderingContext2D = canvasMask!.getContext('2d') as CanvasRenderingContext2D;
+    canvasMask.width = canvasSize.x * pixelSize;
+    canvasMask.height = canvasSize.y * pixelSize;
+    if (pixelSize != 1) {
+      // Draw Canvas Grid
+      for (let i = 0; i < canvasSize.x * pixelSize; i += pixelSize) {
+        ctxMask.fillRect(i, 0, 1, canvasSize.y * pixelSize);
+      }
+      for (let j = 0; j < canvasSize.y * pixelSize; j += pixelSize) {
+        ctxMask.fillRect(0, j, canvasSize.x * pixelSize, 1);
+      }
     }
   }, [pixelSize]);
   function handleMouseMove(e: any) {
@@ -41,17 +55,34 @@ const CanvasBox: React.FC = () => {
   function handleWheel(e: any) {
     const movement = (e.deltaY / 100) * -1;
     const newPixelSize = pixelSize + movement;
-    if (newPixelSize >= 1 && newPixelSize <= 30)
-    setPixelSize(pixelSize + movement);
+    if (newPixelSize >= 1 && newPixelSize <= 40)
+      setPixelSize(pixelSize + movement);
+  }
+  function handleClick(e: any) {
+    const canvas: HTMLCanvasElement = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx: CanvasRenderingContext2D = canvas!.getContext('2d') as CanvasRenderingContext2D;
+    const canvasRect = canvas.getBoundingClientRect();
+    const positionX = e.clientX - canvasRect.x;
+    const positionY = e.clientY - canvasRect.y;
+    const offsetX = positionX % pixelSize;
+    const offsetY = positionY % pixelSize;
+    ctx.fillRect(positionX - offsetX, positionY - offsetY, pixelSize, pixelSize);
   }
   return (
-    <div className='editorBar editorCanvas' onMouseMove={handleMouseMove} onWheel={handleWheel}>
-      <canvas id='canvas' style={{
-        width: `${canvasSize.x * pixelSize}px`,
-        height: `${canvasSize.y * pixelSize}px`,
-        left: `${canvasMovement.x}px`,
-        top: `${canvasMovement.y}px`
-      }}></canvas>
+    <div className='editorBar editorCanvas' onMouseMove={handleMouseMove} onWheel={handleWheel} onClick={handleClick}>
+      <div className='canvasBox' style={{
+          width: `${canvasSize.x * pixelSize}px`,
+          height: `${canvasSize.y * pixelSize}px`,
+          left: `${canvasMovement.x}px`,
+          top: `${canvasMovement.y}px`
+        }}>
+        <canvas id='canvas' style={{
+          width: `${canvasSize.x * pixelSize}px`,
+          height: `${canvasSize.y * pixelSize}px`
+        }}>
+        </canvas>
+        <canvas id='canvasMask'></canvas>
+      </div>
     </div>
   )
 }
